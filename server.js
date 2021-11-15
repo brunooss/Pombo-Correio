0c5c529e7c0f5cf9b75335b21fe9ded1f8a27f52
Bruno Oliveira <bruno.oss@outlook.com>
2021-11-15 00:01:56 -0300

    bugfix

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const path = require('path');
 
+const withCSS = require('@nebular/theme')
+
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/angular-project'));
-app.use(express.static(__dirname + '/node_modules/@nebular/theme/styles/themes'));
+app.use(withCSS);
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/angular-project/'}),
